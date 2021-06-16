c6c8edef8fb6c9ad69a3f115d0f24a6f5f9fdae3
BethanyFolino <bethany.folino@gmail.com>
2021-06-16 13:53:39 -0700

    Now passing all tests and each method in each class has a docstring

--- nodict.py
+++ nodict.py
@@ -3,7 +3,7 @@
 Implementation of the NoDict assignment
 """
 
-__author__ = 'Bethany Folino with help from Jacob Short'
+__author__ = 'Bethany Folino with help from Jacob Short and Matt Perry'
 references = """https://www.pythontutorial.net/python-oop/python-__eq__/#:~:text=
                Python%20automatically%20calls%20the%20__,
                the%20__eq__%20method.
@@ -13,29 +13,50 @@ references = """https://www.pythontutorial.net/python-oop/python-__eq__/#:~:text
 
 class Node:
     def __init__(self, key, value=None):
+        """
+        This method initializes key, value, and hash
+        """
         self.hash = hash(key)
         self.key = key
         self.value = value
 
     def __repr__(self):
+        """
+        This method makes outputs be readable by humans
+        """
         return f"{self.__class__.__name__}({self.key}, {self.value})"
 
     def __eq__(self, other):
+        """
+        This method compares keys to check to see if they are the same
+        """
         return self.key == other.key
 
 
 class NoDict:
 
     def __init__(self, num_buckets=10):
+        """
+        This method initializes num_buckets and a list of lists called
+        buckets
+        """
         buckets = [[] for _ in range(num_buckets)]
         self.num_buckets = num_buckets
         self.buckets = buckets
 
     def __repr__(self):
+        """
+        This method makes outputs legible to human eyes
+        """
         return "\n".join([f'{self.__class__.__name__}.{i}:{bucket}' for i,
                          bucket in enumerate(self.buckets)])
 
     def add(self, key, value):
+        """
+        This makes an index by using hash of each Node with modulo and
+        num_buckets, then compares current Node to Nodes in bucket_node,
+        then removes the original if it finds a match or just adds it if not
+        """
         new_node = Node(key, value)
         index = new_node.hash % self.num_buckets
         for bucket_node in self.buckets[index]:
@@ -44,17 +65,27 @@ class NoDict:
         self.buckets[index].append(new_node)
 
     def get(self, key):
+        """
+        This uses hash to make an index like in add, but uses it to find
+        the key of a matching node and return the corresponding value
+        """
         node_to_find = Node(key)
         index = node_to_find.hash % self.num_buckets
         for node_holder in self.buckets[index]:
             if node_holder == node_to_find:
-                return node_to_find.value
+                return node_holder.value
         raise KeyError(f"{key} not found")
 
     def __getitem__(self, key):
-        # Your code here
-        return
+        """
+        This magic method allows square brackets to be used for
+        assignment by calling the get method that was made above
+        """
+        return self.get(key)
 
     def __setitem__(self, key, value):
-        # Your code here
-        return
+        """
+        This magic method allows square bracket assigned items to be
+        read properly by calling the previously created add method
+        """
+        self.add(key, value)
